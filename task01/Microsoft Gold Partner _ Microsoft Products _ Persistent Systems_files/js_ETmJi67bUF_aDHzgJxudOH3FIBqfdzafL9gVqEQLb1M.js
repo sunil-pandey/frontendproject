a8e36570cc52760e57eaad832cf9d62084dc87d2
Sunil Kumar Pandey <sunil_pandey@persistent.co.in>
2014-08-18 13:00:32 +0530

    task01

---
+++ task01/Microsoft Gold Partner _ Microsoft Products _ Persistent Systems_files/js_ETmJi67bUF_aDHzgJxudOH3FIBqfdzafL9gVqEQLb1M.js
@@ -0,0 +1,142 @@
+(function ($) {
+
+Drupal.behaviors.initColorbox = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+    $('.colorbox', context)
+      .once('init-colorbox')
+      .colorbox(settings.colorbox);
+  }
+};
+
+{
+  $(document).bind('cbox_complete', function () {
+    Drupal.attachBehaviors('#cboxLoadedContent');
+  });
+}
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.initColorboxStockholmsyndromeStyle = {
+  attach: function (context, settings) {
+    $(document).bind('cbox_open', function () {
+      // Hide close button initially.
+      $('#cboxClose', context).css('opacity', 0);
+    });
+    $(document).bind('cbox_load', function () {
+      // Hide close button. (It doesn't handle the load animation well.)
+      $('#cboxClose', context).css('opacity', 0);
+    });
+    $(document).bind('cbox_complete', function () {
+      // Show close button with a delay.
+      $('#cboxClose', context).fadeTo('fast', 0, function () {$(this).css('opacity', 1)});
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.initColorboxLoad = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+    $.urlParams = function (url) {
+      var p = {},
+          e,
+          a = /\+/g,  // Regex for replacing addition symbol with a space
+          r = /([^&=]+)=?([^&]*)/g,
+          d = function (s) { return decodeURIComponent(s.replace(a, ' ')); },
+          q = url.split('?');
+      while (e = r.exec(q[1])) {
+        e[1] = d(e[1]);
+        e[2] = d(e[2]);
+        switch (e[2].toLowerCase()) {
+          case 'true':
+          case 'yes':
+            e[2] = true;
+            break;
+          case 'false':
+          case 'no':
+            e[2] = false;
+            break;
+        }
+        if (e[1] == 'width') { e[1] = 'innerWidth'; }
+        if (e[1] == 'height') { e[1] = 'innerHeight'; }
+        p[e[1]] = e[2];
+      }
+      return p;
+    };
+    $('.colorbox-load', context)
+      .once('init-colorbox-load', function () {
+        var params = $.urlParams($(this).attr('href'));
+        $(this).colorbox($.extend({}, settings.colorbox, params));
+      });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.initColorboxInline = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+    $.urlParam = function(name, url){
+      if (name == 'fragment') {
+        var results = new RegExp('(#[^&#]*)').exec(url);
+      }
+      else {
+        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+      }
+      if (!results) { return ''; }
+      return results[1] || '';
+    };
+    $('.colorbox-inline', context).once('init-colorbox-inline').colorbox({
+      transition:settings.colorbox.transition,
+      speed:settings.colorbox.speed,
+      opacity:settings.colorbox.opacity,
+      slideshow:settings.colorbox.slideshow,
+      slideshowAuto:settings.colorbox.slideshowAuto,
+      slideshowSpeed:settings.colorbox.slideshowSpeed,
+      slideshowStart:settings.colorbox.slideshowStart,
+      slideshowStop:settings.colorbox.slideshowStop,
+      current:settings.colorbox.current,
+      previous:settings.colorbox.previous,
+      next:settings.colorbox.next,
+      close:settings.colorbox.close,
+      overlayClose:settings.colorbox.overlayClose,
+      maxWidth:settings.colorbox.maxWidth,
+      maxHeight:settings.colorbox.maxHeight,
+      innerWidth:function(){
+        return $.urlParam('width', $(this).attr('href'));
+      },
+      innerHeight:function(){
+        return $.urlParam('height', $(this).attr('href'));
+      },
+      title:function(){
+        return decodeURIComponent($.urlParam('title', $(this).attr('href')));
+      },
+      iframe:function(){
+        return $.urlParam('iframe', $(this).attr('href'));
+      },
+      inline:function(){
+        return $.urlParam('inline', $(this).attr('href'));
+      },
+      href:function(){
+        return $.urlParam('fragment', $(this).attr('href'));
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
